155ff80924b5fea3ab7658008df49fcbb87496a4
Vladimir-dev-start <ova.pb@icloud.com>
2021-04-05 23:52:58 +0300

    Initial Commit

---
+++ Swift-start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-start
+//
+//  Created by Владимир on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
